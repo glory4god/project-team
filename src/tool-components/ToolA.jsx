cd4b91a30b66c78df7927e2c23f147e635ffcf52
Hayoung <gkduddy0814@gmail.com>
2021-04-01 14:48:40 +0900

    new-project
    Responsive Web design and when click image, video start

---
+++ src/tool-components/ToolA.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Root = styled.div``;
+
+const ToolA = () => {
+  return (
+    <Root>
+      <div></div>
+    </Root>
+  );
+};
+
+export default ToolA;
